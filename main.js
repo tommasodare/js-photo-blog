c4f1c2d015639a8bcaf7b0b8b353258b5daf4399
tommaso.dare <tommaso.dare96@gmail.com>
2025-02-28 14:27:38 +0100

    Add data.response

--- main.js
+++ main.js
@@ -1,14 +1,21 @@
-fetch("https://lanciweb.github.io/demo/api/pictures/")
+// Seleziono gli elementi della DOM
+const cardEl = document.querySelector(".card")
+const imageEl = document.querySelector(".image")
+const dateEl = document.querySelector(".date")
+const placeEl = document.querySelector(".place")
+
+console.log(cardEl, imageEl, dateEl, placeEl);
+
+// Fetch API
+fetch("https://lanciweb.github.io/demo/api/pictures")
 .then(response => response.json())
 .then(data => {
-    console.log(data);
+    console.log(data.response);
+
+    const [imageValue, dateValue, palceValue] = data.response
+
+
 })
 .catch(error => {
     console.error(error)
 })
-
-// Seleziono gli elementi della DOM
-const cardEl = document.querySelector(".card")
-const imageEl = document.querySelector(".image_box")
-const placeEl = document.querySelector(".place")
-const dateEl = document.querySelector(".date")
